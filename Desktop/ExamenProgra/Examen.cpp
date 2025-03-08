d259765411234fbea5e350d66e5a82b83c4f6d23
Daryl Campos <dcamposd2@miumg.edu.gt>
2025-03-08 10:46:18 -0600

    ejercicio 2.1 realizado por Daryl Moises Campos Diaz 0909-24-17747

---
+++ Desktop/ExamenProgra/Examen.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+using namespace std; 
+
+int main(){
+    int  a = 0, b = 0;
+
+    for (int i = 0; i <= 50; i += 2) {
+        a += i;
+    }
+    cout << "La suma de los numeros pares hasta 50 es: " << a << endl;
+
+    for (int o = 3; o <= 50; o += 3) {
+        b += o;
+    }
+    cout << "La suma de los numeros impares hasta 50 es: " << b << endl;
+}
